a68092c96111483d477d2fe33d3a6da62576d02d
ttejeda <ttejeda@ucol.mx>
2022-02-27 15:55:20 -0600

    string interpolation

--- app.js
+++ app.js
@@ -4,3 +4,9 @@ let logGreeting = (miParametro) => miParametro();
 logGreeting(function () {
     console.log('Hello from a function created on the fly');
 });
+
+//Parte 2: Interpolación de strings
+let logGreeting1 = (miNombre, miColor) => {
+    console.log("¡Hola " + miNombre + " buenos días! Tu color favorito es el " + miColor);
+}
+logGreeting1("Tirzo", "naranja");
